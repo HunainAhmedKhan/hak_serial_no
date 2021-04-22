6621516af8c4f426ad913a5055b38c1c288357ce
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2021-04-22 14:47:18 +0500

    Add files via upload

---
+++ hak_serial_no/__manifest__.py
@@ -0,0 +1,25 @@
+
+{
+    'name': 'Serial number in Sale Order,Purchase Order,Invoice',
+    'version': '13.0.0.0',
+    'author': 'HAKsolutions',
+    'summary': 'Serial number in Sale Order Linee',
+    'description': """This module helps to show serial number in sale order lines.""",
+    'category': 'Base',
+    'website': 'https://www.iwesabe.com/',
+    'license': 'AGPL-3',
+
+    'depends': ['sale_management','account','purchase'],
+
+    'data': [
+        'views/sale_order_views.xml',
+        'report/sale_order_report.xml',
+    ],
+
+    'qweb': [],
+    'images': ['static/description/iWesabe-Apps-Serial-Number-SO.png'],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
